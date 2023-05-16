f8a41ddf366bc3e4c18efba4a1c438b37cb58aba
Daniel Rhodes <danielrhodes47@gmail.com>
2023-05-15 21:15:14 -0500

    first commit

---
+++ 06_single_letter_count/single_letter_count.py
@@ -0,0 +1,23 @@
+def single_letter_count(word, letter):
+    """How many times does letter appear in word (case-insensitively)?
+    
+        >>> single_letter_count('Hello World', 'h')
+        1
+        
+        >>> single_letter_count('Hello World', 'z')
+        0
+        
+        >>> single_letter_count("Hello World", 'l')
+        3
+    """
+    # count = 0
+    # word_lower = word.lower()
+    # letter_lower = letter.lower()
+    # for char in word_lower:
+    #     if char == letter_lower:
+    #         count += 1
+
+    # return count
+    
+    
+    return word.lower().count(letter.lower())
